ec1153a0301c1eb56811d18e0cebe1fcc7de38f9
Kiran Mohan <kiranmohan@123.com>
2015-09-29 23:33:31 +0530

    Formatting

--- arquillian-cdi/src/main/java/org/ktest/study/arquillian/cdi/Greeter.java
+++ arquillian-cdi/src/main/java/org/ktest/study/arquillian/cdi/Greeter.java
@@ -6,11 +6,11 @@ import java.io.PrintStream;
  * A component for creating personal greetings.
  */
 public class Greeter {
-    public void greet(PrintStream to, String name) {
-        to.println(createGreeting(name));
-    }
+	public void greet(PrintStream to, String name) {
+		to.println(createGreeting(name));
+	}
 
-    public String createGreeting(String name) {
-        return "Hello, " + name + "!";
-    }
+	public String createGreeting(String name) {
+		return "Hello, " + name + "!";
+	}
 }
