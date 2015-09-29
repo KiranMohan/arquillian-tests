553e2ac3dcdb1fc9a21e2732f96b5c7e1e501945
Kiran Mohan <kiranmohan@123.com>
2015-09-29 23:34:16 +0530

    add runwith arquillian.class to GreeterTest

--- arquillian-cdi/src/test/java/org/ktest/study/arquillian/cdi/GreeterTest.java
+++ arquillian-cdi/src/test/java/org/ktest/study/arquillian/cdi/GreeterTest.java
@@ -5,11 +5,14 @@ import static org.junit.Assert.*;
 import javax.inject.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(Arquillian.class)
 public class GreeterTest {
 	
 	@Deployment
